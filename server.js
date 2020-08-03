67e980b96f65de9d5639f44f15db40577caba02e
ashutosh2329 <sonutiwari2329@gmail.com>
2020-08-03 23:46:52 +0530

    changes

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-const express = require('express')
-const app = express()
-var details = {
-	'Name':"Ashutosh",
-	'Hobby':"Programming",
-	'status':"Student",
-};
-var contact = {
-	'email':"sonutiwari2329@gmail.com",
-	'phone-number':"6388554494",
-	'address':"varanasi",
-};
-app.get("/",function(request, response){
-	response.send("<h1>hello World!</h1>");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/",function(req, res){
+	res.sendFile(__dirname + "/index.html");
 });
-app.get("/contact",function(request, response){
-	response.send(contact);
+app.post("/",function(req, res){
+	var number1 = Number(req.body.num1);
+	var number2 = Number(req.body.num2);
+    var result = number1 + number2;
+    console.log(result);
+	res.send("The calculated result is " + result);
 });
-app.get("/about",function(request, response){
-	response.send(details);
-});
-app.get("/college",function(request, response){
-	response.send("<h2>Tezpur University</h2>");
-});
+
 
 app.listen(3000, function(){
 	console.log("server started at 3000 port address");
