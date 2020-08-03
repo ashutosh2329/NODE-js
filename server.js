1f77d63f7b1b0f991e13f0573262dd7a4757e511
ashutosh2329 <sonutiwari2329@gmail.com>
2020-08-04 00:21:32 +0530

    bmi calculator

--- server.js
+++ server.js
@@ -9,11 +9,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/",function(req, res){
 	res.sendFile(__dirname + "/index.html");
 });
+app.get("/bmiCalculator",function(req, res){
+	res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmiCalculator",function(req, res){
+	var weight = Number(req.body.weight);
+	var height = Number(req.body.height);
+    var bmi = Math.round(weight/(height*height));
+	res.send("The calculated BMI is " + bmi);
+});
 app.post("/",function(req, res){
 	var number1 = Number(req.body.num1);
 	var number2 = Number(req.body.num2);
     var result = number1 + number2;
-    console.log(result);
 	res.send("The calculated result is " + result);
 });
 
